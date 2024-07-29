7e60b76b74abbb7095b16c2a2037b11abd1249b4
Elham Miandej <87191036+elham-miandej@users.noreply.github.com>
2024-07-29 14:47:14 -0400

    COR-506: Added githubAction to convert csv to resx (#1)
    * COR-506: Added githubAction to convert csv to resx
    
    * COR-506: remove extra lines
    
    * COR-506: Resolve Conversations
    
    * COR-506: Resolve Conversations
    
    * COR-506: Format
    
    * COR506: Add DockerFile

---
+++ ActionInputs.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+namespace MyBlueprint.ResxConverter
+{
+    public class ActionInputs
+    {
+        [Option('i', "input-dir",
+            Required = true,
+            HelpText = "Directory path containing the CSV files you want to convert.")]
+        public string InputDirectory { get; set; } = null!;
+
+        [Option('o', "output-dir",
+             Required = true,
+             HelpText = "Directory where the output ResX files should be saved.")]
+        public string OutputDirectory { get; set; } = null!;
+    }
+
+}
