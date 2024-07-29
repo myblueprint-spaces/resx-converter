de212444ea74828efb324efa63310e6e57f82c18
elham-miandej <elham.miandej@myblueprint.ca>
2024-07-29 12:54:04 -0400

    COR-506: Added githubAction to convert csv to resx

---
+++ resx_convertor/ActionInputs.cs
@@ -0,0 +1,34 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResX_convertor
+{
+    public class ActionInputs
+    {
+        string _repositoryName = null!;
+        string _branchName = null!;
+
+        public ActionInputs()
+        {
+            if (Environment.GetEnvironmentVariable("GREETINGS") is { Length: > 0 } greetings)
+            {
+                Console.WriteLine(greetings);
+            }
+        }
+
+        [Option('d', "dir",
+            Required = true,
+            HelpText = "Input Csv file Directory that you want to convert.")]
+        public string InputDirectory { get; set; } = null!;
+
+        [Option('d', "dir",
+             Required = true,
+             HelpText = "Output ResX file Directory.")]
+        public string OutputDirectory { get; set; } = null!;
+    }
+
+}
---
+++ resx_convertor/Program.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Resources.NetStandard;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using CommandLine;
+using static CommandLine.Parser;
+
+namespace ResX_convertor
+{
+    public class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            using IHost host = Host.CreateDefaultBuilder(args).Build();
+            ParserResult<ActionInputs> parser = Default.ParseArguments<ActionInputs>(() => new(), args);
+            parser.WithNotParsed(
+                errors =>
+                {
+                    host.Services
+                        .GetRequiredService<ILoggerFactory>()
+                        .CreateLogger("DotNet.GitHubAction.Program")
+                        .LogError("{Errors}", string.Join(
+                            Environment.NewLine, errors.Select(error => error.ToString())));
+
+                    Environment.Exit(2);
+                });
+
+            await parser.WithParsedAsync(
+                async options => await ConvertToResX(options));
+
+            await host.RunAsync();
+        }
+
+        public static async ValueTask ConvertToResX(ActionInputs options)
+        {
+            var fileName = "PcspResources.Csv";
+            var fullPath = Path.Combine(options.InputDirectory, fileName);
+            Dictionary<string, string> keyValuePairs = new Dictionary<string, string>();
+            var allLines = File.ReadAllLines(fullPath);
+            foreach (var line in allLines)
+            {
+                var columns = line.Split(new[] { ',' }, 3);
+                if (columns.Length == 3)
+                {
+                    string key = $"{columns[0].Trim()}.{columns[1].Trim()}";
+                    string value = columns[2].Trim().TrimStart('N', '\'').TrimEnd('\'');
+                    keyValuePairs[key] = value;
+                }
+            }
+            var outputFileName = "PcspResources.resx";
+            var outputFullPath = Path.Combine(options.OutputDirectory, outputFileName);
+            using (ResXResourceWriter resx = new ResXResourceWriter(outputFullPath))
+            {
+                foreach (var item in keyValuePairs)
+                {
+                    resx.AddResource(item.Key, item.Value);
+
+
+                }
+                resx.Generate();
+                resx.Close();
+            }
+
+            Console.WriteLine("Done");
+            await ValueTask.CompletedTask;
+            Environment.Exit(0);
+        }
+    }
+}
